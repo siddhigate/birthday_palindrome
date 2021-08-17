a5850487c0b9b65ed0b52e712ab7741dbb05ed12
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-08-17 12:38:35 +0530

    fix: output fix

--- app.js
+++ app.js
@@ -278,13 +278,15 @@ function setLoading(){
 
 function setOutput(status,  format,  pal_date, count){
 
-    let result_date = getDateAsString(pal_date);
+    
     switch (status) {
         case "isPalindrome":
+          let result_date = getDateAsString(pal_date);
             output_div.innerHTML = `<div><img src='./assets/happy.svg'></div><div style="margin:auto; font-size:1.4rem">Yayy! Your birthday is a palindrome:) <span style="font-size:0.5rem">in ${format} format</span></div>`
             break;
 
         case "nextPalindrome":
+            let result_date = getDateAsString(pal_date);
             output_div.innerHTML = `<div><img src='./assets/sad.svg'></div><div style="margin:auto;">Oops! The nearest palindrome date is ${result_date.day}-${result_date.month}-${result_date.year} <span> in ${format} format</span>, you missed by ${count} days. </div>`;
             break;
 
